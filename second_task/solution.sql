526979602c1bb931eacd80cae4ae9f9e32eb5ab8
waterwa1ker <ibragimov_kep@mail.ru>
2023-09-22 16:42:25 +0300

    second task done

--- second_task/solution.sql
+++ second_task/solution.sql
@@ -1,19 +1,21 @@
-create table first_table(
+drop table if exists first_table, second_table, third_table, fourth_table;
+
+create table if not exists first_table(
     first_column int,
     second_column varchar(100)
 ) with (fillfactor = 50);
 
-create table second_table(
+create table if not exists second_table(
     first_column int,
     second_column varchar(100)
 ) with (fillfactor = 75);
 
-create table third_table(
+create table if not exists third_table(
     first_column int,
     second_column varchar(100)
 ) with (fillfactor = 90);
 
-create table fourth_table(
+create table if not exists fourth_table(
     first_column int,
     second_column varchar(100)
 ) with (fillfactor = 100);
@@ -23,5 +25,18 @@ insert into second_table values (generate_series(1, 10000), repeat('B', 50));
 insert into third_table values (generate_series(1, 10000), repeat('C', 50));
 insert into fourth_table values (generate_series(1, 10000), repeat('D', 50));
 
-update first_table set (second_column = concat(second_column, second_column))
+EXPLAIN ANALYZE update first_table set second_column = concat(second_column, second_column)
 where first_column > 0;
+EXPLAIN ANALYZE update second_table set second_column = concat(second_column, second_column)
+where first_column > 0;
+EXPLAIN ANALYZE update third_table set second_column = concat(second_column, second_column)
+where first_column > 0;
+EXPLAIN ANALYZE update fourth_table set second_column = concat(second_column, second_column)
+where first_column > 0;
+
+SELECT nspname || '.' || relname AS "relation",
+    pg_size_pretty(pg_relation_size(C.oid)) AS "size"
+  FROM pg_class C
+  LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+  WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+  ORDER BY pg_relation_size(C.oid) DESC;
